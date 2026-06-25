ec821c4b4099cb6a6c6d776f96c1ec53629d422f
SANVED ANKUSH BARKADE <barkadesanved@gmail.com>
2026-06-25 17:11:52 +0530

    Add files via upload

---
+++ Exception1.py
@@ -0,0 +1,13 @@
+import Marvellousa
+
+def main():
+    print("Enter first numebr : ")
+    No1 = int(input())
+
+    print("Enter second numebr : ")
+    No2 = int(input())
+
+    print("Addition is : ",Ans)
+    
+if __name__ == "__main__":
+    main()
